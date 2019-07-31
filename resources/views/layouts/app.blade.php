367c5c40c3b88a63c5df5601a6018124e8f5ebcf
anilprasad <anilprasad@jmangroup.com>
2019-07-31 15:27:46 +0530

    Jenkins testing push_1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
 
-
-
     <title>{{ config('app.name', 'DevOps Release Calendar') }}</title>
     <link rel="icon" href="{{asset('/img/devops.png')}}" style="" type="image/x-icon">
     <link rel="stylesheet" href="{{asset('/css/timeline.css')}}" >
